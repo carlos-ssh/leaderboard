25ebeb6ae108a98c7ee32b968a43d788fb8775a8
Carlos-SSH <carlos.anezeh@me.com>
2021-09-02 17:24:06 -0500

    Add listObject for display the list of scores.
    Add form file where i will print the form using es6.
    
    create dataApi where i will call the api using axios.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
 import './style.css';
+import listScore from './components/listOjects';
+import form from './components/form';
+
+listScore();
+form();
---
+++ src/components/form.js
@@ -0,0 +1,28 @@
+const form = (submitData) => {
+  const form = document.getElementById('form-container');
+  const inputName = document.createElement('input');
+  const inputScore = document.createElement('input');
+  const button = document.createElement('button');
+
+  inputName.id = 'name';
+  inputName.placeholder = 'Add your Name';
+  inputScore.id = 'score';
+  inputScore.placeholder = 'Add your Score';
+
+  button.innerText = 'Submit';
+  button.id = 'form-btn';
+  button.classList.add('form-btn');
+  button.onclick = submitScore;
+
+  form.append(inputName);
+  form.append(inputScore);
+  form.append(button);
+
+  function submitScore() {
+    const name = document.getElementById('name').value;
+    const score = document.getElementById('score').value;
+    submitData(name, score);
+  }
+};
+
+export default form;
